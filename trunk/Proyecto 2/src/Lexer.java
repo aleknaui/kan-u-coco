54eb8b88a8547a81be13b378284f801fcb43ee2d
ale.knaui@gmail.com <ale.knaui@gmail.com@704127d9-94fa-caf6-fdbc-838701f2040b>
2011-04-21 20:53:21 +0000

    Se borró el recognizer y sus clases relacionadas, ya que no funcionaban ni iban a servir :/ enfoque 2: enfoque harder empezado. Adaptadas las constantes de los operadores de RegEx para poder trabajar con cualquier caracter, junto con sus pruebas en la clase Main del Proyecto 1

---
+++ trunk/Proyecto 2/src/Lexer.java
@@ -0,0 +1,13 @@
+/**
+ * Esta clase es el analizador sintáctico que se
+ * construye poco a poco añadiendo diferentes reconocedores
+ * de tokens (en forma de AFDs mínimos) con una pequeña variación
+ * de el algoritmo de Thompson.
+ * 
+ * La idea inicial era 
+ * @author ale
+ *
+ */
+public class Lexer {
+
+}
